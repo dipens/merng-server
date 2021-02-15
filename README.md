aede5db0f4f67f64c3439879b39903e18f0d8e96
dipens <dipenmshah8@gmail.com>
2021-02-15 13:46:22 -0500

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# MERNG = MongoDB, Express using Apollo Server, React, Node, graphQL
+
+
+This is a sample project demonstrating usages of MERNG
---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MONGODB:'mongodb+srv://dipen:rCP8VK8QOWK4NEsC@cluster0.tqdvf.mongodb.net/test?retryWrites=true&w=majority',
+    SECRET_KEY: 'some very secret key'
+}
